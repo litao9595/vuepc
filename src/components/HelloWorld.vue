6a5f5fec381ef2a339d13f1d6e8cfbb44da77f9b
陈宝林 <505951628@qq.com>
2020-03-12 14:45:51 +0800

    logs

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="ceshi">文字</div>
+    <el-button>默认按钮</el-button>
+  </div>
+</template>
+
+<script>
+import {configUrl} from './js/configUrl'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created(){
+    console.log(configUrl)
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" rel="stylesheet/less" scoped>
+@import "css/style.less";
+.ceshi{
+  color: @color;
+}
+</style>
---
+++ src/components/js/configUrl.js
@@ -0,0 +1,7 @@
+const hot="www.";
+const configUrl={
+  kk:hot+'daidu.com',
+};
+export {
+  configUrl
+}
